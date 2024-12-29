4128ca28afe6b5bf5f45a1c1d8295a421d9b1ff2
Anubhuti Srivastava <anubhutisrivastava312@gmail.com>
2024-12-29 20:59:56 +0530

    learned how to use props and pass it to children components from parent components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from './components/card';
 import Button from './components/Button';
 import Button2 from './components/Button2';
+import Students from './components/Students';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       <Card />
       <Button/>
       <Button2/>
+      <Students name="Doraemon!" age={24} isStudent={true}/>
+      <Students name="Shinchan!" age={5} isStudent={false}/>
+
 
 
     </div>
